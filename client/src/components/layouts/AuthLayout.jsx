b4474681e6787f5efe7fdfddd85eac278fb9c607
Ishwarsing34 <igirase65@gmail.com>
2026-02-09 00:57:17 +0530

    #1

--- client/src/components/layouts/AuthLayout.jsx
+++ client/src/components/layouts/AuthLayout.jsx
@@ -1,15 +1,28 @@
+import React from "react";
+import CARD_2 from "../../assets/images/card2.png";
+
 const AuthLayout = ({ children }) => {
   return (
-    <div>
-      <div className="screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
-        <h2 className="text-lg font-medium text-black">
+    <div className="flex h-screen w-full">
+      {/* Left section */}
+      <div className="flex w-full flex-col justify-center px-12 lg:w-[60%]">
+        <h2 className="mb-6 text-2xl font-semibold text-black">
           Expense Tracker
         </h2>
         {children}
+      </div>
+
+      {/* Right section */}
+      <div className="hidden h-full w-[40%] items-center justify-center bg-violet-50 lg:flex">
+        
+        <img
+          src={CARD_2}
+          alt="Auth Illustration"
+          className="w-[80%] max-w-md"
+        />
       </div>
     </div>
   );
 };
 
 export default AuthLayout;
-
