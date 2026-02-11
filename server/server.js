51da753f4f714c6f204bcac61cf82fb058587071
Ishwarsing34 <igirase65@gmail.com>
2026-02-12 03:32:29 +0530

    #

--- server/server.js
+++ server/server.js
@@ -1,68 +1,49 @@
-import dns from 'dns'
-
-dns.setServers([
-    "8.8.8.8",
-    "8.8.4.4",
-    "1.1.1.1"
-]);
-
-
-
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./configs/db.js";
-import path from 'path';
-import authRouter from "./routes/authRoutes.js"
-import incomeRouter from './routes/incomeRoutes.js';
-import expenseRouter from './routes/ExpenseRoutes.js';
-import dashRouter from './routes/dashboardRoutes.js';
 
+import authRouter from "./routes/authRoutes.js";
+import incomeRouter from "./routes/incomeRoutes.js";
+import expenseRouter from "./routes/expenseRoutes.js"
+import dashRouter from "./routes/dashboardRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/exports", express.static(path.join(process.cwd(), "exports")));
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      process.env.CLIENT_URL,
+    ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders:["Content-Type", "Authorization"]
   })
 );
 
-
-
+app.use("/exports", express.static(path.join(process.cwd(), "exports")));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 connectDB();
 
-
 app.get("/", (req, res) => {
-  res.send( "Server is running 🚀" );
+  res.send("Server is running 🚀");
 });
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/income", incomeRouter )
-app.use("/api/v1/dashboard",dashRouter)
-app.use("/api/v1/expense", expenseRouter)
-
-
-
-app.use("/uploads", express.static("uploads"));
-
-
-
+app.use("/api/v1/income", incomeRouter);
+app.use("/api/v1/dashboard", dashRouter);
+app.use("/api/v1/expense", expenseRouter);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-
-
